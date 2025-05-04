cfd9e38aba0ac91a40c51a39406845db57b155f3
Tong <sm29729443@gmail.com>
2025-05-05 06:40:27 +0800

    initial authorization module

---
+++ ec-authorization/src/test/java/com/ecommercetong/ecauthorization/EcAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercetong.ecauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
