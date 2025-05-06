870959870e568272dc8a886a447cf5b2f3baa1f9
Tong <sm29729443@gmail.com>
2025-05-07 04:06:27 +0800

    使http res status 與 ApiResponse status一致

--- ec-application/src/main/java/com/ecommercetong/exceptionhandler/ControllerExceptionHandler.java
+++ ec-application/src/main/java/com/ecommercetong/exceptionhandler/ControllerExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.ecommercetong.exceptionhandler;
 
-import com.ecommercetong.exception.CustomizedException;
 import com.ecommercetong.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,13 +7,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
-public class GlobalExceptionHandler {
-
+@ControllerAdvice(basePackages = "com.ecommercetong.controller")
+public class ControllerExceptionHandler {
     @ExceptionHandler
-    public ResponseEntity<?> handleException(CustomizedException e) {
-        return ResponseEntity.status(e.getHttpStatus()).body(ApiResponse
-                .wrapperResponse(e.getHttpStatus(),e.getMessage(), null));
+    public ResponseEntity<?> handleException(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse
+                .wrapperResponse(HttpStatus.BAD_REQUEST,e.getFieldError().getDefaultMessage(), null));
     }
-
 }
